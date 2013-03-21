7aa78f68183041c2c662824d5a67579429c2faf0
Luiza Nunes <luhhsnunes@gmail.com>
2013-03-21 20:01:09 -0300

    Adding variables names

--- find_bus_time.rb
+++ find_bus_time.rb
@@ -1,18 +1,27 @@
 require 'watir'
 
+@url = "http://wap.rmtcgoiania.com.br/"
+@text_field_name = "txtStopName"
+@text_field_value = "3658"
+@button_name = "btnAsk"
+@bus_line_number = "305"
+@how_many_minutes = "50"
+@success_message = "Previsto para os proximos 50min"
+@fail_message = "Nao ha previsao para os proximos 50min"
+
 browser= Watir::Browser.new :chrome
 
-browser.goto "http://wap.rmtcgoiania.com.br/"
-browser.text_field(:name=>"txtStopName").set "3658"
-browser.button(:name => "btnAsk").click
+browser.goto(@url)
+browser.text_field(:name=> @text_field_name).set(@text_field_value)
+browser.button(:name => @button_name).click
 
 browser.tables.each do |table|
   table.rows.each do |row|
-    if row.cells[0].text == "305" 
-      if row.cells[-1].text <= "50"
-        puts "Previsto para os proximos 50min"
+    if row.cells[0].text == @bus_line_number 
+      if row.cells[-1].text <= @how_many_minutes      	
+        puts @success_message
       else
-        puts "Nao ha previsao para os proximos 50min"
+        puts @fail_message
       end
     end
   end
