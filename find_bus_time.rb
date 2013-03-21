db1893c68c6b43bb207ce524a00816d3bd57786c
Luiza Nunes <luhhsnunes@gmail.com>
2013-03-20 22:48:20 -0300

    Refactoring

--- find_bus_time.rb
+++ find_bus_time.rb
@@ -1,17 +1,33 @@
 require 'watir'
-b= Watir::Browser.new :chrome
-b.goto "http://wap.rmtcgoiania.com.br/"
-b.text_field(:name=>"txtStopName").set "3658"
-b.button(:name => "btnAsk").click
-b.tables.each do |table|
-	table.rows.each do |row|
-		if row.cells[0].text == "305" 
-			if row.cells[-1].text <= "50"
-				puts "Previsto para os proximos 50min"
-			else
-				puts "Nao ha previsao para os proximos 50min"
-			end
-		end
-	end
+
+browser= Watir::Browser.new :chrome
+
+browser.goto "http://wap.rmtcgoiania.com.br/"
+
+browser.text_field(:name=>"txtStopName").set "3658"
+
+browser.button(:name => "btnAsk").click
+
+browser.tables.each do |table|
+
+  table.rows.each do |row|
+
+    if row.cells[0].text == "305" 
+
+      if row.cells[-1].text <= "50"
+
+        puts "Previsto para os proximos 50min"
+
+      else
+
+        puts "Nao ha previsao para os proximos 50min"
+
+      end
+
+    end
+
+  end
+
 end
-b.close
+
+browser.close
