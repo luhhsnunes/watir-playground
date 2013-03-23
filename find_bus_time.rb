a798af232d66fc2e3c655034a0d6c156d400f97f
Luiza Nunes <luhhsnunes@gmail.com>
2013-03-22 22:04:07 -0300

    Refactoring and adding page objects

--- find_bus_time.rb
+++ find_bus_time.rb
@@ -1,30 +1,13 @@
-require 'watir'
+load 'find_bus_time.rb'
 
-@url = "http://wap.rmtcgoiania.com.br/"
-@text_field_name = "txtStopName"
-@text_field_value = "3658"
-@button_name = "btnAsk"
+@bus_stop_number = "3658"
 @bus_line_number = "305"
 @how_many_minutes = "50"
-@success_message = "Previsto para os proximos 50min"
-@fail_message = "Nao ha previsao para os proximos 50min"
 
-browser= Watir::Browser.new :chrome
+browser = inicialize(browser)
 
-browser.goto(@url)
-browser.text_field(:name=> @text_field_name).set(@text_field_value)
-browser.button(:name => @button_name).click
+search(browser,@bus_stop_number)
 
-browser.tables.each do |table|
-  table.rows.each do |row|
-    if row.cells[0].text == @bus_line_number 
-      if row.cells[-1].text <= @how_many_minutes      	
-        puts @success_message
-      else
-        puts @fail_message
-      end
-    end
-  end
-end
+find_bus_time(browser,@bus_line_number,@how_many_minutes)
 
-browser.close
+close_browser(browser)
---
+++ find_bus_time_pg_obj.rb
@@ -0,0 +1,46 @@
+require 'watir'
+
+@url = "http://wap.rmtcgoiania.com.br/"
+@text_field_name = "txtStopName"
+@button_name = "btnAsk"
+@success_message = "Previsto para os proximos 50min"
+@fail_message = "Nao ha previsao para os proximos 50min"
+
+def inicialize(browser)
+ 
+ browser= Watir::Browser.new :chrome
+ browser.goto(@url)
+ 
+ return browser
+
+end
+
+def search(browser,bus_stop_number)
+ 
+ browser.text_field(:name=> @text_field_name).set @bus_stop_number
+ browser.button(:name => @button_name).click
+
+end
+
+def find_bus_time(browser,line_number,minutes)
+
+ browser.tables.each do |table|
+  table.rows.each do |row|
+    if row.cells[0].text == line_number 
+
+      if row.cells[-1].text <= minutes      	
+        puts @success_message
+      else
+        puts @fail_message
+      end
+
+    end
+  end
+ end
+end
+
+def close_browser(browser)
+
+ browser.close
+ 
+end
